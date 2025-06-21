51c691d9522a1b5c8fbfeff54bd3191157f5294c
manueldavids <manuelsantanadev@proton.me>
2025-06-21 11:45:16 -0400

    Add User model with attributes for first name, last name, email, and admin status :D

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,12 @@
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+
+    def __str__(self):
+        return f"User(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, email={self.email}, is_admin={self.is_admin})"
