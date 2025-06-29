ec943121b2e15a419c063a27e3b334b10e0360fd
manueldavids <manuelsantanadev@proton.me>
2025-06-29 16:13:01 -0400

    Update the create_app() method to receive a configuration

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -8,6 +8,7 @@ from app.api.v1.reviews import api as reviews_ns
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    app.config.from_object(config_class)
 
     # Register the namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
