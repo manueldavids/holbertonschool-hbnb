589511bc3e29edb23df96ff309eeb4a74595569f
manueldavids <manuelsantanadev@proton.me>
2025-06-29 17:49:22 -0400

    Add reviews namespace to API for managing reviews functionality

--- part3/api/__init__.py
+++ part3/api/__init__.py
@@ -22,7 +22,9 @@ api = Api(
 from api.v1.auth import api as auth_api
 from api.v1.users import api as users_api
 from api.v1.places import api as places_api
+from api.v1.reviews import api as reviews_api
 
 api.add_namespace(auth_api, path='/auth')
 api.add_namespace(users_api, path='/users')
 api.add_namespace(places_api, path='/places')
+api.add_namespace(reviews_api, path='/reviews')
