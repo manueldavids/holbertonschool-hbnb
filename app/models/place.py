f1c107e019fa637ad8f5cb0717aa868b7433244c
manueldavids <manuelsantanadev@proton.me>
2025-07-04 13:13:13 -0400

    fix: set id and owner_id as String(36) and update association table in Place

---
+++ app/models/place.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
+db = SQLAlchemy()
+
+class Place(db.Model):
+    __tablename__ = 'places'
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    owner_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+
+    # ... rest of the model definition ... 
+
+place_amenity = db.Table(
+    'place_amenity',
+    db.Column('place_id', db.String(36), db.ForeignKey('places.id'), primary_key=True),
+    db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
+) 
