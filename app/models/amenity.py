a1150c348b7848774608dd070fcceca3b690249b
manueldavids <manuelsantanadev@proton.me>
2025-07-04 13:12:40 -0400

    fix: set id as String(36) with UUID default in User

---
+++ app/models/amenity.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
+db = SQLAlchemy()
+
+class Amenity(db.Model):
+    __tablename__ = 'amenities'
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f'<Amenity {self.name}>' 
