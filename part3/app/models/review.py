a2f7505a4d0f2910512eda3750f5bb0dcee77f4d
manueldavids <manuelsantanadev@proton.me>
2025-07-04 12:28:47 -0400

    Added place_id and user_id as foreign keys to the Review model, establishing relationships with Place and User

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -5,4 +5,7 @@ class Review(BaseModel, db.Model):
     __tablename__ = 'reviews'
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.Text)
+    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+    # Relationship with Place and User handled by backref
     # place_id y user_id se agregan en Task 8
