cd7a8b67db78874a66878179cd8c4a3207bb4ffa
manueldavids <manuelsantanadev@proton.me>
2025-07-23 17:29:19 -0400

    Add main entry point script to run the HBnB Flask application.

--- part3/run.py
+++ part3/run.py
@@ -6,6 +6,6 @@ Simple script to run the Flask application.
 from app import create_app
 
 if __name__ == '__main__':
-    app = create_app('development')
+    app = create_app()
     print(app.url_map)
     app.run(host='127.0.0.1', port=5000, debug=True) 
